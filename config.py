aeac1b884c5ebfe70e038173adbb00af9c67c0a3
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-11 16:09:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@
 import logging
 from telethon import TelegramClient
 from os import getenv
-from FLASH.data import DEV
+from STORM.data import DEV
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
